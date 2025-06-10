66a887dadf8d1cfd1e3f66a5f05a229a9b07de93
Saif Daghsni <daghsnisaif@gmail.com>
2025-06-10 14:10:05 +0100

    first step

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import './App.css'
-import { useState } from 'react';
-
-
+import Login from './login-signup/login';
+/*step 1*/
 function App() {
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
-  }
   return (
-    <>  
-        <div>
-          <h1>Login</h1>
-          <p>Welcome to Golden Spoon</p>
-          <form onSubmit={handleSubmit}>
-            <h4>E-mail</h4>
-            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            <h4>Password</h4>
-            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-          </form>
-          <button type="submit">Next</button>
-        </div>
-    </>
-  )
+    <Login />
+  );
 }
 
-export default App
-
+export default App;
